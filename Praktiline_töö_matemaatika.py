caa23dda057544e6769cf8454c99822e2439e51c
MarekLukk <mareklukk8@gmail.com>
2025-04-05 14:16:50 +0300

    On lisatud tsykkel

--- Praktiline_töö_matemaatika.py
+++ Praktiline_töö_matemaatika.py
@@ -1,79 +1,74 @@
 #Praktiline töö matemaatika
-
 import random
 
+while True:
+    print("Teadmiste kontroll, Matemaatika")
+    tase1 = ['+', '-']
+    tase2 = ['*', '/']
+    tase3 = ['**']
 
-print("teadmiste kontroll, Matemaatika")
-tase1 = ['+', '-']
-tase2 = ['*', '/']
-tase3 = ['**']
+    valik = input("Vali ülesannete keerukus: tase1, tase2 või tase3 => ").strip()
 
-valik = input("Vali ülesannete keerukus:tase1, tase2 või tase3 => ").strip()
+    if valik not in ["tase1", "tase2", "tase3"]:
+        print("VIGA! Vale ülesannete keerukus!")
+        continue
 
-if valik not in ["tase1", "tase2", "tase3"]:
-    print("VIGA! Vale ülesannete keerukus!")
-    exit()
-
-
-näited = 5
-õiged_näited = 0    
-for i in range(näited):
-    number1=random.randint(1, 100)
-    number2=random.randint(1, 50)
+    näited = 5
+    õiged_näited = 0    
+    for i in range(näited):
+        number1 = random.randint(1, 100)
+        number2 = random.randint(1, 50)
     
-    if valik=="tase1":
-        operation = random.choice(tase1)
-    elif valik =="tase2":
-        operation = random.choice(tase2)
-    elif valik =="tase3":
-        operation = random.choice(tase3)
+        if valik == "tase1":
+            operation = random.choice(tase1)
+        elif valik == "tase2":
+            operation = random.choice(tase2)
+        elif valik == "tase3":
+            operation = random.choice(tase3)
+    
+        if operation == '+':
+            õige_vastus = number1 + number2
+            print(f"{number1} + {number2} = ")
+        elif operation == '-':
+            õige_vastus = number1 - number2
+            print(f"{number1} - {number2} = ")
+        elif operation == '*':
+            õige_vastus = number1 * number2
+            print(f"{number1} * {number2} = ")
+        elif operation == '/':
+            õige_vastus = number1 / number2
+            print(f"{number1} / {number2} = ")
+        elif operation == '**':  
+            number2 = random.randint(1, 3)  
+            õige_vastus = number1 ** number2  
+            print(f"{number1} ** {number2} = ") 
+    
+        try:
+            õpilase_vastus = float(input("Teie vastus: "))
+            if õpilase_vastus == õige_vastus:
+                print("Õige!")
+                õiged_näited += 1
+            else:
+                print(f"Vale. Õige vastus: {õige_vastus}")
+        except ValueError:
+            print("Palun sisestage number.")
+
+    arve = (õiged_näited / näited) * 100
+
+    if arve < 60:
+        hinne = 2
+    elif 60 <= arve < 75:
+        hinne = 3
+    elif 75 <= arve < 90:
+        hinne = 4
     else:
-        print("VALE ülesannete keerukus!")    
-    if operation == '+':
-        õige_vastus = number1 + number2
-        print(f"{number1} + {number2} = ")
-    elif operation == '-':
-        õige_vastus = number1 - number2
-        print(f"{number1} - {number2} = ")
-    elif operation == '*':
-        õige_vastus = number1 * number2
-        print(f"{number1} * {number2} = ")
-    elif operation == '/':
-        õige_vastus = number1 / number2
-        print(f"{number1} / {number2} = ")
-    elif operation == '**':  
-        number2 = random.randint(1, 3)  
-        õige_vastus = number1 ** number2  
-        print(f"{number1} ** {number2} = ") 
-    else:
-        print("Vali õige tase!")
+        hinne = 5
+
+    print(f"Sa vastasid õigesti {õiged_näited} / {näited} .")
+    print(f"Sinu hinne: {hinne}")
     
-    
-    try:
-        õpilase_vastus = float(input("Teie vastus: "))
-        if õpilase_vastus == õige_vastus:
-            print("Õige!")
-            õiged_näited += 1
-        else:
-            print(f"Vale. Õige vastus: {õige_vastus}")
-    except ValueError:
-        print("Palun sisestage number.")
+    uuesti = input("Kas soovid uuesti proovida? (jah/ei): ").strip().lower()
+    if uuesti != "jah":
+        print("Aitäh mängimast! Nägemist!")
+        break
 
-arve = (õiged_näited / näited) * 100
-
-if arve < 60:
-    hinne = 2
-elif 60 <= arve < 75:
-    hinne = 3
-elif 75 <= arve < 90:
-    hinne = 4
-else:
-    hinne = 5
-
-print(f"Sa vastasid õigesti {õiged_näited} / {näited} .")
-print(f"Sinu hinne: {hinne}")
-
-
-
-        
-        
